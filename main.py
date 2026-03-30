60d9bdd736b8b5f072d855d4fb3af852a32df57d
Ilya Balatsenko <senkoet@gmail.com>
2026-03-30 07:35:20 -0400

    Minor changes

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ total = 0
 special_nums = []
 palindromes = []
 
-def palindrome(num):
-    num_str = str(num)
-    if num_str == num_str[::1]:
-        palindromes.append(num_str)
 
-num = 12221
-palindrome(num)
+# def timer(num):
+
+
+def isPalindrome(num):
+    num_str = str(num)
+    if (num_str[0] == num_str[-1] and num_str[1] == num_str[-2]): # since its only a 5 digit number, we can hard code this
+        return True
+
+num = 14041
+isPalindrome(num)
 print(palindromes)
