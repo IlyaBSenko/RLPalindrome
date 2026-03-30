b6b792aa82f0e165dbc1c692c83ce4e34cd58ffe
Ilya Balatsenko <senkoet@gmail.com>
2026-03-30 07:48:49 -0400

    minor progress

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ special_nums = []
 palindromes = []
 
 
+# also need to acount for overtime
+# def special_nums(num):
+    # num_str = str(num)
+    
+
+
 # def timer(num):
 
 
+# also account for overtime
 def isPalindrome(num):
     num_str = str(num)
     if (num_str[0] == num_str[-1] and num_str[1] == num_str[-2]): # since its only a 5 digit number, we can hard code this
         return True
 
-num = 14041
+
+num = 15001
+starting_num = "05000" # since integers cant start with 0, and we need a starting number
 isPalindrome(num)
 print(palindromes)
