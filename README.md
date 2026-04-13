0116d2be2a2d15301df896cb8b42537ddee15fa0
Ilya Balatsenko <senkoet@gmail.com>
2026-04-13 17:06:41 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# RLPalindrome
+## Ever wondered how many palindromes exist in the rocket league scoreboard / timer?
+## Well we can write code to find out
+
+### This is a fun problem because this number is most often an odd number
+### The number takes the form of team1 + minute + seconds(2 digits) + team2
+### where team1, team2 are 0-9 and the timer counts m:ss with seconds 0-59.
+
+
+#### Since we know that the 1st and last number will be 0 - 9
+#### We can write a nested for loop for both numbers
+```python
+for team1 in range(10):               
+    for team2 in range(10): 
+```
+
+#### The middle number is a little tricky but uses the same logic
+#### We write a number nested for loop inside the first, but with minutes for the first number 
+#### and seconds as the second and third number
+
+```python
+for minutes in range(10):     
+            for seconds in range(60):  
+```
+
+#### Now we need to format the number into a string and then check if that number is a palindrome
+
+```python
+s = f"{team1}{minutes}{seconds:02d}{team2}"
+
+                if s == s[::-1]:
+                    palindromes.append(s)
+```
+
+#### So the final product ends up being:
+```python
+import time
+
+palindromes = []
+
+for team1 in range(10):               
+    for team2 in range(10):            
+        for minutes in range(10):    
+            for seconds in range(60):  
+                
+                s = f"{team1}{minutes}{seconds:02d}{team2}"
+
+                if s == s[::-1]:
+                    palindromes.append(s)
+
+print(f"Found {len(palindromes)} palindromes")
+```
+
--- main.py
+++ main.py
@@ -1,62 +1,23 @@
-# sarting variables
-length = 5
-total = 0
-special_nums = []
+# import time
+
 palindromes = []
-# TODO: Maybe account for overtime? Would be hard since that goes to POS Infinity
 
+# Huuuuuge nested loop, but its chill because it's only 10*10*10*60 = 60,000 iterations
+for team1 in range(10):                # 0-9
+    for team2 in range(10):            # 0-9
+        for minutes in range(10):      # 0-9
+            for seconds in range(60):  # 0-59
+                
+                s = f"{team1}{minutes}{seconds:02d}{team2}"
 
+                if s == s[::-1]:
+                    palindromes.append(s)
 
-# def special_nums(num):
-    # num_str = str(num)
-    
+print(f"Found {len(palindromes)} palindromes")
 
-
-# needs cleanup bad
-def timer(timer_num):
-    timer_num_int = int(timer_num)
-    left = timer_num_int // 100 # account for when a number becomes 99, then there is no "hundreds" digit of a three digit number
-    middle = (timer_num_int % 100) // 10
-    right = timer_num_int % 10
-    timer_num_int = str(left) + str(middle) + str(right)
-    while (timer_num_int != 000):
-        timer_num_int = int(timer_num_int)
-        timer_num_int -= 1
-        if (middle >= 6):
-            middle = 5
-        if (right == 0):
-            middle -= 1
-            if (right == 0 and middle == 0):
-                left -= 1
-        timer_num_int = str(left) + str(middle) + str(right)
-
-
-        return str(timer(timer_num_int))
-
-
-
-
-def isPalindrome(num_str):
-    # print(num_str)
-    return (num_str[0] == num_str[-1] and num_str[1] == num_str[-2]) # since its only a 5 digit number, we can hard code this
-
-
-
-def main(num_str):
-    total = 0
-    timer_num = '500'
-    yerd = isPalindrome(timer(timer_num))
-    if (yerd):
-        palindromes.append(num_str)
-        total += 1
-    print(palindromes)
-
-
-
-team1 = '0'
-timer_num = '500'
-team2 = '0'
-num_str = team1 + timer_num + team2
-print(num_str)
-main(num_str)
+# if you want to see the specific palindromes
+# for p in palindromes:
+    # print(p)
+    # time.sleep(.005)
+    # then just import time at the top
 
